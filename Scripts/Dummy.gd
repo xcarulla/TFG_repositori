9be4650ef9e83184b250808dea35a60c80322f08
xcarulla <xaviercarullapoch@hotmail.com>
2024-04-18 02:56:53 +0200

    Dummy pt.2
    some animations and fixes

--- Scripts/Dummy.gd
+++ Scripts/Dummy.gd
@@ -3,39 +3,34 @@ extends CharacterBody3D
 
 # Variables:
 const MOVE_SPEED = 5
-const JUMP_FORCE = 12
+const JUMP_FORCE = 5
 const GRAVITY = 10
-const MAX_FALL_SPEED = 30
 var y_vel = 0
 var x_vel = 0
 var facing_right = true
 
 @onready var anim_tree = $anim_tree
 
-func _ready():
-	pass # Replace with function body.
-
-
-func _process(delta):
+func _physics_process(delta):
 	var move_dir = 0
 	var grounded = is_on_floor()
 	var just_jumped = false
 	
+	# Movement
 	if Input.is_action_pressed("game_move_right"):
 		move_dir += 1
 	if Input.is_action_pressed("game_move_left"):
 		move_dir -= 1
-	
+	velocity.x = move_dir * MOVE_SPEED
 	move_and_slide()
 	
-	y_vel -= GRAVITY * delta
-	if y_vel < -MAX_FALL_SPEED:
-		y_vel = -MAX_FALL_SPEED
+	# Gravity
+	if not is_on_floor():
+		velocity.y -= GRAVITY * delta
 		
 	if grounded:
-		y_vel = -0.1
 		if Input.is_action_pressed("game_jump"):
-			y_vel = JUMP_FORCE
+			velocity.y += JUMP_FORCE
 			just_jumped = true
 	
 	if move_dir < 0 and facing_right:
