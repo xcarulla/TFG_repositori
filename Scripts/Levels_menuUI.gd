9462b43d4913c744ff4d9df7bd6c850d982e73d4
xcarulla <xaviercarullapoch@hotmail.com>
2024-08-07 14:15:54 +0200

    Stars final
    scprit perque la UI representi cuantes estrelles s'han agafat

---
+++ Scripts/Levels_menuUI.gd
@@ -0,0 +1,26 @@
+extends Control
+
+var starsDict
+
+func _ready():
+	starsDict = GlobalVariables.getStars(name)
+	setStarsVisibility()
+
+func setStarsVisibility():
+	var star = find_child("Star1")
+	if starsDict["Star1"]:
+		star.self_modulate = "#ffcb00"
+	else:
+		star.self_modulate = "#195598"
+	
+	star = find_child("Star2")
+	if starsDict["Star2"]:
+		star.self_modulate = "#ffcb00"
+	else:
+		star.self_modulate = "#195598"
+	
+	star = find_child("Star3")
+	if starsDict["Star3"]:
+		star.self_modulate = "#ffcb00"
+	else:
+		star.self_modulate = "#195598"
--- Scripts/playMenu.gd
+++ Scripts/playMenu.gd
@@ -2,6 +2,7 @@ extends Control
 
 @export var anim_player: AnimationPlayer
 
+
 func _return():
 	anim_player.play("hidePlay")
 
